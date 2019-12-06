211cf6ad64a2dbef051c6176c6cc30ed2a4f3269
gurkodil <id15gwm@cs.umu.se>
2019-12-06 03:35:05 +0100

    day4 + merge

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -3,31 +3,24 @@ use std::io::prelude::*;
 mod lib;
 
 fn main() {
-    let mut file = File::open("data/03.dat")
-        .expect("Can not open file...");
-    
-    let mut content = String::new(); 
-     
+    let mut file = File::open("data/03.dat").expect("Can not open file...");
+
+    let mut content = String::new();
+
     file.read_to_string(&mut content)
         .expect("Failed reading content from file.");
 
     let wires = content.split("\n").collect::<Vec<&str>>();
-    
+
     if wires.len() < 2 {
-        return
+        return;
     }
 
-    let res_part_one = lib::part_one(
-        wires[0].to_string(),
-        wires[1].to_string()
-        );
+    let res_part_one = lib::part_one(wires[0].to_string(), wires[1].to_string());
 
     println!("Part one: {}", res_part_one);
-    
-    let res_part_two = lib::part_two(
-        wires[0].to_string(),
-        wires[1].to_string()
-        );
+
+    let res_part_two = lib::part_two(wires[0].to_string(), wires[1].to_string());
 
     println!("Part two: {}", res_part_two);
 }
